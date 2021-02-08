8c9a4c420842985cba7181ea5effa7cac83d038e
Lee Spitzley <lspitzley@albany.edu>
2021-02-08 14:10:47 -0500

    lab to print my name and contents of current directory
    lab for 2/8

---
+++ 1pm/echo_lab.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+######################################
+# This script will print out your name
+# Author: Lee Spitzley
+#####################################
+
+echo "Hi, I'm Lee Spitzley"
+
+# print the contents of the current directory
+echo $(ls) 
