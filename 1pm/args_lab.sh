5503bfb53e2e8d41a1da88e45f4c692015909e5d
Lee Spitzley <lspitzley@albany.edu>
2021-02-10 14:06:23 -0500

    1pm version of args lab

---
+++ 1pm/args_lab.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+ 
+#############################
+
+echo "The number of arguments is $#"
+
+echo "The first argument is $1"
+
+echo "Argument \$2 is $2"
+
+echo "Argument \$0 is $0"
+
+echo "Argument \$9 is $9"
+
+echo "All arguments are accessed with \$* : $*"
+
+# access the arguments
+
+# add the two numbers
+
+# print the sum
+
+# multiply the two numbers
+
+# print the product
