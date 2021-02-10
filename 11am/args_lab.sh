0e646654109763439308e27a32b6d965ed245d36
Lee Spitzley <lspitzley@albany.edu>
2021-02-10 12:42:23 -0500

    created lab file for arguments lab
    also updated gitignore

---
+++ 11am/args_lab.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "The number of arguments is:" $# 
+echo "The first argument is: " $1
+
+# prints the name of the script
+echo "The \$0 is: $0"
+
+# add two numbers
+
+# print the results of addition
+
+# multiply the two numbers
+
+# print the results of multiplication
+
+# done!
