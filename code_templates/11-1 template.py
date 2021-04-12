a0dccb0801fb062a1878a1b727139204b1fe6ffd
lspitzley <lspitzley@albany.edu>
2021-04-12 11:20:02 -0400

    code & data for 11-1

---
+++ code_templates/11-1 template.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 15:36:46 2021
+
+Play with reddit data on ML/AI
+
+
+Primary data source
+https://www.kaggle.com/maksymshkliarevskyi/reddit-data-science-posts
+
+@author: Lee Spitzley
+"""
+
+#%% imports
+import os
+import pandas as pd
+
+
+#%% read data
+
+# list of files
+
+
+# empty list to store dataframes
+
+
+# read in dataframes
+
+
+# join them together
+# https://stackoverflow.com/questions/32444138/concatenate-a-list-of-pandas-dataframes-together
+
+# get a random sample of data to see if things work
+
+
+
+#%% explore data
+
+
